23d8ab3b1cefa9de723a5f8c6c6e970018cb794c
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-03-06 21:55:07 +0530

    3 -Navigating to Destinations with NavController & SafeArgs | Android Navigation Component

--- app/src/main/java/com/createsapp/hetpacknavigationexample/HomeFragment.kt
+++ app/src/main/java/com/createsapp/hetpacknavigationexample/HomeFragment.kt
@@ -5,7 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        btn_view_balance.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToViewBalanceFragment()
+            navController.navigate(action)
+        }
+
+        btn_transactions.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToViewTransactionsFragment()
+            navController.navigate(action)
+        }
+
+        btn_send_money.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToChooseReceiverFragment()
+            navController.navigate(action)
+        }
+    }
 }
